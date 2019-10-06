47177d09c84b9b8dee55233729efd398f202056b
jonathan <bitbucket@jmartz.de>
2019-10-06 13:07:58 +0200

    use default database and auth parameters

--- middelware/Authenticate.php
+++ middelware/Authenticate.php
@@ -1,83 +1,81 @@
 <?php
 
-	namespace App\Http\Middleware;
+namespace App\Http\Middleware;
 
-	use App\Http\Controllers\Controller;
-	use App\User;
-	use Closure;
-	use Illuminate\Contracts\Auth\Factory as Auth;
-	use Illuminate\Support\Facades\Validator;
-	use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\User;
+use Closure;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
-	/**
-	 * Class Authenticate
-	 *
-	 * @package App\Http\Middleware
-	 */
-	class Authenticate extends Controller
-	{
-		/**
-		 * The authentication guard factory instance.
-		 *
-		 * @var \Illuminate\Contracts\Auth\Factory
-		 */
-		protected $auth;
+/**
+ * Class Authenticate
+ *
+ * @package App\Http\Middleware
+ */
+class Authenticate extends Controller
+{
+    /**
+     * The authentication guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
 
-		/**
-		 * Create a new middleware instance.
-		 *
-		 * @param  \Illuminate\Contracts\Auth\Factory  $auth
-		 * @return void
-		 */
-		public function __construct(Auth $auth)
-		{
-			$this->auth = $auth;
-			parent::__construct();
-		}
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @return void
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+        parent::__construct();
+    }
 
-		/**
-		 * Handle an incoming request.
-		 *
-		 * @param  \Illuminate\Http\Request  $request
-		 * @param  \Closure  $next
-		 * @param  string|null  $guard
-		 * @return mixed
-		 */
-		public function handle($request, Closure $next, $guard = null)
-		{
-			$validator = Validator::make($request->all(), [
-				'username' => 'required',
-				'token' => 'required|size:512'
-			])->validate();
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $validator = Validator::make($request->all(), [
+            'auth.username' => 'required',
+            'auth.token' => 'required|size:512'
+        ])->validate();
 
-			$users = DB::connection('mysql.write')
-					   ->table('users')
-					   ->where('username','=',$request->input('username'))
-					   ->where('username_hash','=',sha1($request->input('username')));
+        $users = DB::table('users')
+            ->where('username','=',$request->input('auth.username'))
+            ->where('username_hash','=',sha1($request->input('auth.username')));
 
-			$count = $users->count();
+        $count = $users->count();
 
-			$user = $users->first();
+        $user = $users->first();
 
-			if($count === 1){
-				$tokens = DB::connection('mysql.write')
-							->table('auth_tokens')
-							->where('UID','=',$user->id)
-							->where('token','=',$request->input('token'));
-				if($tokens->count() === 1){
-					return $next($request);
-				}
-				else{
-					$this->addResult('status', 'error');
-					$this->addResult('message', 'Token doesnt exists.');
-					return $this->getResponse();
-				}
-			}
-			else{
-				$this->addResult('status', 'error');
-				$this->addResult('message', 'User doesnt exists.');
-				return $this->getResponse();
-			}
-		}
+        if($count === 1){
+            $tokens = DB::table('auth_tokens')
+                ->where('UID','=',$user->id)
+                ->where('token','=',$request->input('auth.token'));
+            if($tokens->count() === 1){
+                return $next($request);
+            }
+            else{
+                $this->addResult('status', 'error');
+                $this->addResult('message', 'Token doesnt exists.');
+                return $this->getResponse();
+            }
+        }
+        else{
+            $this->addResult('status', 'error');
+            $this->addResult('message', 'User doesnt exists.');
+            return $this->getResponse();
+        }
+    }
 
-	}
+}
